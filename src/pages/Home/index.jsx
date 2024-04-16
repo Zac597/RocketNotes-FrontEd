b17a08a4ee4b0e5453d48de669a66487898a36dc
Zac597 <isaaclima597@gmail.com>
2024-04-16 15:23:04 -0300

    feat: creating tags

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,6 @@
+import { useState, useEffect } from 'react'
+
+import { api } from '../../services/api'
 import { FiPlus, FiSearch } from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 import { Header } from '../../components/Header'
@@ -9,37 +12,50 @@ import { ButtonText } from '../../components/ButtonText'
 
 
 export function Home() {
+  const [tags, setTags] = useState([])
+
+
+  useEffect(() => {
+    async function fetchTags() {
+      const response = await api.get("/tags")
+      setTags(response.data)
+    }
+
+    fetchTags()
+  }, [])
+
   return (
     <Container>
       <Brand>
-      <h1>Rocketnotes</h1>
+        <h1>Rocketnotes</h1>
       </Brand>
 
-      <Header/>
-      
-      <Menu>
-        <li>
-          <ButtonText title="Todos" isActive/>
-        </li>
-        <li>
-          <ButtonText title="React"/>
-        </li>
-        <li>
-          <ButtonText title="Nodejs"/>
-        </li>
+      <Header />
 
+      <Menu>
+        <li><ButtonText title="Todos" isActive /></li>
+
+        {
+          tags && tags.map(tag => (
+            <li key={String(tag.id)}>
+              <ButtonText
+                title={tag.name}
+              />
+            </li>
+          ))
+        }
       </Menu>
       <Search>
-        <Input placeholder="Pesquisar pelo título" icon={FiSearch}/>
+        <Input placeholder="Pesquisar pelo título" icon={FiSearch} />
       </Search>
       <Content>
         <Section name="Minhas notas">
-          <Notes data={{ name:'React', tags: [{id: '1', name: 'react'}, {id: '2', name:'Rocketseat'}] }}/>
+          <Notes data={{ name: 'React', tags: [{ id: '1', name: 'react' }, { id: '2', name: 'Rocketseat' }] }} />
         </Section>
       </Content>
       <NewNote to="/new">
-        <FiPlus/>
-          Criar Nota
+        <FiPlus />
+        Criar Nota
       </NewNote>
     </Container>
   )
