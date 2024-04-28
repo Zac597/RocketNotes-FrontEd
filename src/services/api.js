ba614e4f4f51ece5da966ad72cafb7ace5fb374e
Zac597 <isaaclima597@gmail.com>
2024-04-28 09:21:23 -0300

    feat: update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333"
+  baseURL: "https://api-rocketnotes-o3c8.onrender.com"
 })
 
 api.get("/users/:id")
